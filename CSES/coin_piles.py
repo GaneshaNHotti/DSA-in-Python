33594dfa34798e01c4870877d12eac5fc736efe9
Ganesha N Hotti <ganeshahotti5112000@gmail.com>
2026-05-05 18:56:33 +0530

    add solution for CSES problem: Coin Piles

---
+++ CSES/coin_piles.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())
+
+    if (a+b) % 3 == 0 and max(a, b) <= 2 * min(a, b):
+        print("YES")
+    else:
+        print("NO")
+
